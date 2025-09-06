11335ace151eaf606eaee1e5438bb91abc3a856d
ChaosCantrip <dev@chaoscantrip.com>
2025-09-06 06:27:31 +0100

    Reconfigured eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      "brace-style": ["warn", "allman"],
+      "quotes": ["warn", "double"],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  }
+]);
